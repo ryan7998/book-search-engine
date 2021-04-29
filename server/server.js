2749533c1c2c66eb1b716cbe85c05865b8b14a33
fazle <ryan7998@gmail.com>
2021-04-29 00:33:07 -0400

    backend queries and mutation developed

--- server/server.js
+++ server/server.js
@@ -1,29 +1,41 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 //  Implement the Apollo Server and apply it to the Express server as middleware
 const {ApolloServer} = require ('apollo-server-express');
 // const routes = require('./routes');
-const {typeDefs, resolvers} = require('./schemas');
-// create a new Apollo server and pass in our schema data
-const server = new ApolloServer({typeDefs, resolvers});
 
-const app = express();
+const {typeDefs, resolvers} = require('./schemas');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({typeDefs, resolvers, context:authMiddleware});
+
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({app});
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
 // app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -16,8 +16,10 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
+      // return res.status(400).json({ message: 'You have no token!' });
     }
+    
 
     // verify token and get user data out of it
     try {
@@ -25,15 +27,15 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return updated request object
+    return req;
   },
+  
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
